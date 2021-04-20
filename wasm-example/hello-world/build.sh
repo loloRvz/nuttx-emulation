a0bf5bea6334ad832e6f9ad15f6f5a97088deaae
loloRvz <rouvinez.laurent@gmail.com>
2021-04-20 10:30:31 -0300

    making repo changes

--- wasm-example/hello-world/build.sh
+++ wasm-example/hello-world/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /opt/wasi-sdk/bin/clang \
-	-o test.wasm test.c \
+	-o hello.wasm hello.c \
 	-O3 -z stack-size=8192 -nostdlib\
 	-Wl,--initial-memory=65536 \
     -Wl,--strip-all,--no-entry,--allow-undefined \
@@ -9,8 +9,8 @@
     -Wl,--export=__heap_base,--export=__data_end \
     -Wl,--import-memory
 
-wamrc -o test.aot test.wasm
+wamrc -o hello.aot hello.wasm
 
-tftp 192.168.0.224 -c put test.wasm
-tftp 192.168.0.224 -c put test.aot
+tftp 192.168.0.224 -c put hello.wasm
+tftp 192.168.0.224 -c put hello.aot
 
