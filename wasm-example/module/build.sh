15677e1cb89fc1d8b1d72c799dd4cfe095238e0e
loloRvz <rouvinez.laurent@gmail.com>
2021-04-22 16:58:37 -0300

    hello

--- wasm-example/module/build.sh
+++ wasm-example/module/build.sh
@@ -6,6 +6,7 @@
 	-O3 -z stack-size=8192 -nostdlib \
 	-Wl,--initial-memory=65536 \
     -Wl,--strip-all,--no-entry \
+    -Wl,--export=__heap_base,--export=__data_end \
     -Wl,--export=sum \
     -Wl,--import-memory 
 
@@ -13,6 +14,7 @@ wamrc --target=thumbv7 \
 	  --cpu=cortex-a9 \
 	  --cpu-features=armv7-a \
 	  -o module.aot module.wasm
+	  
 
 tftp 192.168.0.224 -c put module.wasm
 tftp 192.168.0.224 -c put module.aot
@@ -22,6 +24,7 @@ wat2wasm -o sum.wasm sum.wat
 wamrc --target=thumbv7 \
 	  --cpu=cortex-a9 \
 	  --cpu-features=armv7-a \
+	  --opt-level=0 \
 	  -o sum.aot sum.wasm
 
 
