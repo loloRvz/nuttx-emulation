b0d35003f61491d9eaa8affefe4b3bc839a6f36a
loloRvz <rouvinez.laurent@gmail.com>
2021-05-21 16:27:44 -0300

    Callback from inside .wasm module works!

--- wasm-example/module/build.sh
+++ wasm-example/module/build.sh
@@ -2,17 +2,17 @@
 
 /opt/wasi-sdk/bin/clang \
 	-o mod.wasm module.c \
-	-O3 -z stack-size=8192 -nostdlib \
+	-z stack-size=8192 -nostdlib \
 	-Wl,--initial-memory=65536 \
     -Wl,--strip-all,--no-entry,--allow-undefined \
     -Wl,--export=sum \
-    -Wl,--export=callcallback \
-    -Wl,--import-memory 
+    -Wl,--export=callfunc \
+	#-Wl,--export-memory 
 
 wamrc --target=thumbv7a \
 	  -o mod.aot mod.wasm
 	  
-wamrc -o mod1.aot mod.wasm
+#wamrc -o mod1.aot mod.wasm
 
 tftp 192.168.0.224 -c put mod.wasm
 tftp 192.168.0.224 -c put mod.aot
@@ -21,9 +21,9 @@ tftp 192.168.0.224 -c put mod.aot
 #wat2wasm -o sum.wasm sum.wat
 
 #wamrc --target=thumbv7a \
-#	   --cpu=cortex-a9 \
-#	   --target-abi=gnueabihf \
-#	   -o sum.aot sum.wasm
+	   --cpu=cortex-a9 \
+	   --target-abi=gnueabihf \
+	   -o sum.aot sum.wasm
 	  
 #tftp 192.168.0.224 -c put sum.wasm
 #tftp 192.168.0.224 -c put sum.aot
--- wasm-example/module/sum.wat
+++ wasm-example/module/sum.wat
@@ -1,9 +1,9 @@
 (module
 	(func $call (import "" "call"))
-	(func (export "callfunc") (call $call))
 	(func $sum (param $x i32) (param $y i32) (result i32)
 		local.get $x
 		local.get $y
 		i32.add)
 	(export "sum" (func $sum))
+	(func (export "callfunc") (call $call))
 )
